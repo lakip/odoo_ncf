fc5fb2f7020e17ff51d26f4940079aff8c54f807
lakip <aronlakip@gmail.com>
2019-07-24 00:48:44 +0300

    @api methods added

--- models/farmer_registration.py
+++ models/farmer_registration.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-from odoo import fields,models
+from odoo import fields,models, api
+from odoo.exceptions import ValidationError
 
 
 class FarmerRegistration(models.Model):
@@ -14,11 +15,30 @@ class FarmerRegistration(models.Model):
     bank_name = fields.Char('Bank Name')
     address_home_id = fields.Char('Address')
     place_of_residence = fields.Char('Place of Residence')
+    initials = fields.Selection(selection=[('MR', 'MR'),
+                                           ('MRS', 'MRS')], onchange='onchange_gender')
     Status = fields.Char('Status')
     gender = fields.Selection(selection=[('male', 'Male'),
-                                         ('female', 'Female')])
+                                         ('female', 'Female')], readonly=True)
     marital = fields.Selection(selection=[('married', 'married'),
                                           ('single', 'single')])
     birthday = fields.Datetime('Birthday')
+
+    @api.multi
+    @api.onchange('initials')
+    def onchange_gender(self):
+        if self.initials == 'MR':
+            self.gender = 'male'
+        elif self.initials == 'MRS':
+            self.gender = 'female'
+        else:
+            ''
+
+    @api.constrains('name', 'berry_number')
+    def check_name(self):
+        for this in self:
+            if self.name == self.berry_number:
+                raise ValidationError('farmers name should not be same with farmers number')
+
 
 
